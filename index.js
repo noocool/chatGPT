5e464e9d8f065870b6cb8e04b3f341cb1bae5c68
noocool <ngothinh287@gmail.com>
2023-03-25 12:16:35 +0700

    fix vercel

--- index.js
+++ index.js
@@ -1,88 +1,74 @@
-const express = require('express');
+const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
-require('dotenv').config();
-
+require("dotenv").config();
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-
 const textGeneration = async (prompt) => {
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Human: ${prompt}\nAI: `,
+      temperature: 0.9,
+      max_tokens: 500,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+      stop: ["Human:", "AI:"],
+    });
 
-    try {
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: `Human: ${prompt}\nAI: `,
-            temperature: 0.9,
-            max_tokens: 500,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0.6,
-            stop: ['Human:', 'AI:']
-        });
-    
-        return {
-            status: 1,
-            response: `${response.data.choices[0].text}`
-        };
-    } catch (error) {
-        return {
-            status: 0,
-            response: ''
-        };
-    }
+    return {
+      status: 1,
+      response: `${response.data.choices[0].text}`,
+    };
+  } catch (error) {
+    return {
+      status: 0,
+      response: "",
+    };
+  }
 };
 
+const app = express();
 
-const webApp = express();
-
-const PORT = process.env.PORT;
-
-webApp.use(express.urlencoded({ extended: true }));
-webApp.use(express.json());
-webApp.use((req, res, next) => {
-    console.log(`Path ${req.path} with Method ${req.method}`);
-    next();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`Path ${req.path} with Method ${req.method}`);
+  next();
 });
 
-
-webApp.get('/', (req, res) => {
-    res.sendStatus(200);
+app.get("/", (req, res) => {
+  return res.send("Welcome to saiyan chat!");
 });
 
+app.post("/dialogflow", async (req, res) => {
+  let action = req.body.queryResult.action;
+  let queryText = req.body.queryResult.queryText;
 
-webApp.post('/dialogflow', async (req, res) => {
-    
-    let action = req.body.queryResult.action;
-    let queryText = req.body.queryResult.queryText;
-
-    if (action === 'input.unknown') {
-        let result = await textGeneration(queryText);
-        if (result.status == 1) {
-            res.send(
-                {
-                    fulfillmentText: result.response
-                }
-            );
-        } else {
-            res.send(
-                {
-                    fulfillmentText: `Sorry, I'm not able to help with that.`
-                }
-            );
-        }
+  if (action === "input.unknown") {
+    let result = await textGeneration(queryText);
+    if (result.status == 1) {
+      res.send({
+        fulfillmentText: result.response,
+      });
     } else {
-        res.send(
-            {
-                fulfillmentText: `No handler for the action ${action}.`
-            }
-        );
+      res.send({
+        fulfillmentText: `Sorry, I'm not able to help with that.`,
+      });
     }
+  } else {
+    res.send({
+      fulfillmentText: `No handler for the action ${action}.`,
+    });
+  }
 });
 
+const port = process.env.PORT || 9001;
 
-webApp.listen(PORT, () => {
-    console.log(`Server is up and running at ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is up and running at ${port}`);
 });
